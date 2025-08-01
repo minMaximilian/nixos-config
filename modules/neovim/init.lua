3c71e9b083d437916a6e6f29f826e2a6f1befb9b
minMaximilian <53843385+minMaximilian@users.noreply.github.com>
2025-08-01 23:58:53 +0100

    added nvim via nixcats

--- modules/neovim/init.lua
+++ modules/neovim/init.lua
@@ -1,47 +1,50 @@
--- Use the nixCats wrapper for lazy.nvim.
--- NOTE: This is the core of the nixCats integration. It's like the normal
--- require("lazy").setup(), but it automatically provides all the plugins
--- you defined in your Nix configuration.
+-- NOTE: this just gives nixCats global command a default value
+-- so that it doesnt throw an error if you didnt install via nix.
+-- usage of both this setup and the nixCats command is optional,
+-- but it is very useful for passing info from nix to lua so you will likely use it at least once.
+require('nixCatsUtils').setup {
+  non_nix_value = true,
+}
+
+-- NOTE: You might want to move the lazy-lock.json file
+local function getlockfilepath()
+  if require('nixCatsUtils').isNixCats and type(nixCats.settings.unwrappedCfgPath) == 'string' then
+    return nixCats.settings.unwrappedCfgPath .. '/lazy-lock.json'
+  else
+    return vim.fn.stdpath 'config' .. '/lazy-lock.json'
+  end
+end
+local lazyOptions = {
+  lockfile = getlockfilepath(),
+}
+
+-- NOTE: this the lazy wrapper. Use it like require('lazy').setup() but with an extra
+-- argument, the path to lazy.nvim as downloaded by nix, or nil, before the normal arguments.
 require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 'lazy.nvim' }, {
-  -- We don't need to list plugins here because Nix is providing them.
-  -- We can, however, configure them by referring to them by their name.
+  { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
+  -- disable mason.nvim while using nix
+  -- precompiled binaries do not agree with nixos, and we can just make nix install this stuff for us.
+  { 'williamboman/mason-lspconfig.nvim', enabled = require('nixCatsUtils').lazyAdd(true, false) },
+  { 'williamboman/mason.nvim', enabled = require('nixCatsUtils').lazyAdd(true, false) },
   {
-    "folke/tokyonight.nvim",
-    -- The `config` function will run after the plugin is loaded.
-    -- lazy.nvim is smart enough to load themes first.
-    config = function()
-      vim.cmd.colorscheme "tokyonight"
-    end,
+    'nvim-treesitter/nvim-treesitter',
+    build = require('nixCatsUtils').lazyAdd ':TSUpdate',
+    opts_extend = require('nixCatsUtils').lazyAdd(nil, false),
+    opts = {
+      -- nix already ensured they were installed, and we would need to change the parser_install_dir if we wanted to use it instead.
+      -- so we just disable install and do it via nix.
+      ensure_installed = require('nixCatsUtils').lazyAdd({ 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' }, false),
+      auto_install = require('nixCatsUtils').lazyAdd(true, false),
+    },
   },
   {
-    "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        -- These two lines are CRITICAL for a read-only Nix environment.
-        -- They tell treesitter to NOT try and install anything itself.
-        ensure_installed = {},
-        auto_install = false,
-        highlight = {
-          enable = true,
-        },
-      })
-    end,
+    'folke/lazydev.nvim',
+    opts = {
+      library = {
+        { path = (nixCats.nixCatsPath or '') .. '/lua', words = { 'nixCats' } },
+      },
+    },
   },
-
-  -- This line will automatically load any .lua files you add to the `lua/plugins/` directory.
-  -- This is the recommended way to keep your configuration organized.
+  -- import/override with your plugins
   { import = 'plugins' },
-}, {}) -- The final {} is for lazy.nvim options, which we don't need.
-
--- Basic Neovim options can go here, as they don't depend on plugins.
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = "a"
-vim.opt.showmode = false
-vim.opt.clipboard = "unnamedplus"
-vim.opt.breakindent = true
-vim.opt.updatetime = 250
-vim.opt.signcolumn = "yes"
-vim.opt.termguicolors = true
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+}, lazyOptions)
